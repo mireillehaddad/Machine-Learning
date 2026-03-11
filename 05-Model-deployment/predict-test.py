5ce9ed362bfbb31f5d16eb23ed457e1cbd868fff
Mireille El Haddad <45281188+mireillehaddad@users.noreply.github.com>
2026-03-11 00:34:01 +0000

    predict test py

--- 05-Model-deployment/predict-test.py
+++ 05-Model-deployment/predict-test.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 get_ipython().run_line_magic('autosave', '0')
 
-
-# In[2]:
-
-
 import requests
 
-
-# In[3]:
-
-
 url = 'http://localhost:9696/predict'
-
-
-# In[4]:
 
 
 customer = {
@@ -45,13 +32,11 @@ customer = {
 }
 
 
-# In[5]:
 
 
 customer
 
 
-# In[6]:
 
 
 response = requests.post(url, json=customer)
@@ -60,19 +45,12 @@ print(response.status_code)
 print(response.text)
 
 
-# In[7]:
-
 
 requests.post(url,json=customer).json()
 
 
-# In[9]:
-
 
 response = requests.post(url, json=customer).json()
-
-
-# In[10]:
 
 
 if response['churn'] == True:
